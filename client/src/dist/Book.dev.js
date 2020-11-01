ad6357063eb65906088e93cd7d16be894c445c84
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2020-11-01 15:48:42 +0100

    Created a Book component

---
+++ client/src/dist/Book.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./Book.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function Book() {}
+
+var _default = Book;
+exports["default"] = _default;
